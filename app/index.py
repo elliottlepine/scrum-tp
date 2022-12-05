63716dd09890fd0b6b55b6b9997ddb60fa207396
Elliott Lépine <elliott.lepine@alumni.univ-avignon.fr>
2022-12-05 17:56:01 +0100

    pdf2text integration

--- app/index.py
+++ app/index.py
@@ -1,7 +1,6 @@
 from adapters.TerminalAdapter import TerminalAdapter
-from adapters.FileSystemAdapter import FileSystemAdapter
 from adapters.SystemAdapter import SystemAdapter
 
-backlog = FileSystemAdapter.getInstance().open(SystemAdapter.getInstance().getArgv())
+# file = FileSystemAdapter.getInstance().open(SystemAdapter.getInstance().getArgv())
 
-TerminalAdapter.basic(backlog.getContent())
+TerminalAdapter.basic(SystemAdapter.getInstance().runCommand("pdftotext -raw " + SystemAdapter.getInstance().getArgv()[1]) + "")
