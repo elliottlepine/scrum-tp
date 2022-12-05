6b5fee344c33a8ba3d8cca633dfb9f6dc4b7f8b4
YacineTrousselle <yacinetrousselle@gmail.com>
2022-12-05 17:32:17 +0100

    bash pdftotext avec arg

---
+++ bash/pdftotext.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+args=("-raw" "-nodiag" "-raw -nodiag")
+
+for pdf in $(ls CORPUS_TRAIN)
+do
+    for i in ${!args[@]};
+    do 
+        echo pdftotext ${args[$i]} 'CORPUS_TRAIN/'$pdf 'CORPUS_TEXT/'$pdf'.'$i'.txt'
+        pdftotext ${args[$i]} 'CORPUS_TRAIN/'$pdf 'CORPUS_TEXT/'$pdf'.'$i'.txt'
+    done 
+done
