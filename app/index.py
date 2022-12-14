28dda84fee6446c1f93c3264420b81eaaf64cfd9
YacineTrousselle <yacinetrousselle@gmail.com>
2022-12-14 09:07:10 +0100

    ajout argument text ou xml

--- app/index.py
+++ app/index.py
@@ -4,6 +4,8 @@ from adapters.SystemAdapter import SystemAdapter
 from models.File import File
 from models.PDF import PDF
 
+import argparse
+
 PDFTOTEXT = "pdftotext -raw"
 PDFTOTEXTTITLE = "pdftotext -bbox-layout -q -l 1"
 TEMP_FILE_PATH = "temp.txt"
@@ -26,8 +28,25 @@ def run_pdftotext(bbox_option: bool = False) -> File:
 
     return file
 
-run_pdftotext(True)
-
-FileSystemAdapter.getInstance().delete(TEMP_FILE_PATH)
+# run_pdftotext(True)
+#
+# FileSystemAdapter.getInstance().delete(TEMP_FILE_PATH)
 
 # print(file.extractAbstract())
+
+def main(args):
+    if args['text']:
+        print('On récupère au format text')
+
+    if args['xml']:
+        print('On récupère au format xml')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Récupère des infos d\'un pdf')
+    action = parser.add_mutually_exclusive_group(required=True)
+
+    action.add_argument('-t', '--text', metavar='pdfFilename', help='Infos au format text')
+    action.add_argument('-x', '--xml', metavar='pdfFilename', help='Infos au format pdf')
+
+    args = parser.parse_args()
+    main(vars(args))
