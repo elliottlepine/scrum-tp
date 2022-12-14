8000c8229106c6a830c6ac35d05391f509b6669b
YacineTrousselle <yacinetrousselle@gmail.com>
2022-12-14 08:55:30 +0100

    function pour lancer la commande

--- app/index.py
+++ app/index.py
@@ -6,26 +6,28 @@ from models.PDF import PDF
 
 PDFTOTEXT = "pdftotext -raw"
 PDFTOTEXTTITLE = "pdftotext -bbox-layout -q -l 1"
-TEMP_FILE_PATH = "/app/temp.txt"
+TEMP_FILE_PATH = "temp.txt"
 
-TerminalAdapter.basic(
-    SystemAdapter.getInstance().runCommand(
-        PDFTOTEXTTITLE
-        + " "
-        + SystemAdapter.getInstance().getArgv()[1]
-        + " "
-        + TEMP_FILE_PATH
+def run_pdftotext(bbox_option: bool = False) -> File:
+    command = PDFTOTEXTTITLE if bbox_option else PDFTOTEXT
+
+    TerminalAdapter.basic(
+        SystemAdapter.getInstance().runCommand(
+            command
+            + " "
+            + SystemAdapter.getInstance().getArgv()[1]
+            + " "
+            + TEMP_FILE_PATH
+        )
     )
-)
+    file = PDF.read(TEMP_FILE_PATH)
+    file.path = SystemAdapter.getInstance().getArgv()[2]
+    file.create()
 
-file = PDF.read(TEMP_FILE_PATH)
+    return file
 
-file.path = SystemAdapter.getInstance().getArgv()[2]
-
-file.create()
-
-print(file.get_title())
+run_pdftotext(True)
 
 FileSystemAdapter.getInstance().delete(TEMP_FILE_PATH)
 
-print(file.extractAbstract())
+# print(file.extractAbstract())
