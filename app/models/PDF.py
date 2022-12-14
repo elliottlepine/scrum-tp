0dd32e71e9909a4df47ff75d2eb68720628f42af
lyesdki <lyes.douki@alumni.univ-avignon.fr>
2022-12-14 10:50:01 +0100

    changer import config

--- app/models/PDF.py
+++ app/models/PDF.py
@@ -5,7 +5,7 @@ from common.runPDFtoText import TEMP_FILE_PATH, runPDFtoText
 import os
 import re
 
-from xml.etree.cElementTree import etree
+from xml.etree import ElementTree as etree
 
 class PDF(File):
     def __init__(self, path: str, file):
