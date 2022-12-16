776ebefc19efabf82d8f4f84e1b59c93790a9615
Elliott Lépine <elliott@brad.ag>
2022-12-16 14:22:57 +0100

    Add extractConclusion method

--- app/models/PDF.py
+++ app/models/PDF.py
@@ -8,6 +8,7 @@ import re
 from xml.etree import ElementTree as root
 from xml.dom import minidom
 
+
 class PDF(File):
     def __init__(self, path: str, file):
         super().__init__(path, file)
@@ -128,16 +129,12 @@ class PDF(File):
         # Return the abstract block by concatenating the individual lines
         return "\n".join(lines[startAbstract:endAbstract])
 
-    
-    
-
     ###
     # Returns corpus
     ###
+
     def extractCorpus(self):
         content = self.content
-
-        print(content)
 
         # Split the text into lines
         lines = content.split("\n")
@@ -166,6 +163,7 @@ class PDF(File):
                 or "CONCLUSION" in line
                 or "Discussion" in line
                 or "DISCUSSION" in line
+                or "References" in line
             ):
                 endCorpus = i
                 break
@@ -188,6 +186,63 @@ class PDF(File):
         return corpus
 
     ###
+    # Returns the conclusion of the paper
+    ###
+    def extractConclusion(self):
+        content = self.content
+
+        print(content)
+
+        # Split the text into lines
+        lines = content.split("\n")
+
+        # Find the index of the first line of the abstract
+        startConclusion = None
+        for i, line in enumerate(lines):
+            if (
+                "Conclusion" in line
+                or "CONCLUSION" in line
+                or "Conclusions" in line
+                or "CONCLUSIONS" in line
+            ):
+                startConclusion = i
+                break
+
+        # If no lines start with "Abstract", return an empty string
+        if startConclusion is None:
+            return ""
+
+        # Find the index of the last line of the abstract
+        endConclusion = None
+        for i, line in enumerate(lines[startConclusion + 1:], startConclusion + 1):
+            if (
+                "Acknowledgements" in line
+                or "ACKNOWLEDGMENT" in line
+                or "Acknowledgments" in line
+                or "Reference" in line
+
+            ):
+                endConclusion = i
+                break
+
+        # If no lines start with "I. " or "1 ", return an empty string
+        if endConclusion is None:
+            return ""
+
+        # Return the abstract block by concatenating the individual lines
+        conclusion = ""
+
+        splittedConclusion = lines[startConclusion:endConclusion]
+
+        for i, line in enumerate(splittedConclusion):
+            conclusion += line + "\n"
+
+            if (line.endswith('.')):
+                conclusion += "\n"
+
+        return conclusion
+
+    ###
     # Parses self.content and converts it to TXT
     ###
     def toTXT(self):
@@ -196,7 +251,8 @@ class PDF(File):
         content += self.extractFileName() + "\n\n"
         content += self.extractTitle() + "\n\n"
         content += self.extractAbstract() + "\n\n"
-        content += self.extractCorpus()
+        content += self.extractCorpus() + "\n\n"
+        content += self.extractConclusion()
 
         self.content = content
 
@@ -206,14 +262,26 @@ class PDF(File):
         root = minidom.Document()
         article = root.createElement("Article")
         root.appendChild(article)
+
         preamble = root.createElement("preamble")
         preamble.setAttribute("preamble", self.extractFileName())
         article.appendChild(preamble)
+
         titre = root.createElement("titre")
         titre.setAttribute("titre", self.extractTitle())
         article.appendChild(titre)
+
         abstract = root.createElement("abstract")
         abstract.setAttribute("abstract", self.extractAbstract())
         article.appendChild(abstract)
-        self.content = root.toprettyxml(indent ="\t") 
+
+        corpus = root.createElement("corpus")
+        corpus.setAttribute("corpus", self.extractCorpus())
+        corpus.appendChild(corpus)
+
+        conclusion = root.createElement("conclusion")
+        conclusion.setAttribute("conclusion", self.extractCorpus())
+        conclusion.appendChild(conclusion)
+
+        self.content = root.toprettyxml(indent="\t")
         return self
